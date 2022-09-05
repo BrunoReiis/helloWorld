e8bb1a01ef7895b8d22234196130db6b1aae3733
Bruno e Marcos <BrunoeMarcos@test.com>
2022-09-05 08:44:18 -0300

    Criação de header,footer e content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'helloMundo';
+
+  numeroCurso:string[] = [];
+  curso = "";
+  paragrafo="";
+
+  addCurso()
+  {
+    this.numeroCurso.push(this.curso)
+  }
+
+  mudarParagrafo()
+  {
+    if(this.numeroCurso.length == 0)
+    {
+      this.paragrafo = "Nâo temos cursos no momento";
+    }
+    if(this.numeroCurso.length >= 1)
+    {
+        this.paragrafo = "Temos cursos disponiveis";
+    }
+    return this.paragrafo;
+  }
+
 }
